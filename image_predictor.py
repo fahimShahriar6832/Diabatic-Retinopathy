cc8e1c13e2f252c75f0ee87cd21a4820c49ef071
Fahim Shahriar <74281428+fahimShahriar6832@users.noreply.github.com>
2022-07-01 19:04:59 +0600

    Update image_predictor.py

--- image_predictor.py
+++ image_predictor.py
@@ -4,12 +4,12 @@ from keras.models import load_model
 import numpy as np
 import joblib
 
-st.header("Image Predictor")
+#st.header("Image Predictor")
 
 html_temp = """
 <div style="background-color:#025246 ;padding:10px">
-<h2 style="color:white;text-align:center;background-color: Blue;">Try again and again!!!</h2>
-<h3 style="color:red;text-align:center;">You will crack it Fahim</h3>
+<h2 style="color:white;text-align:center;background-color: Blue;">Image Predictor</h2>
+
 </div>
 """
 st.markdown(html_temp, unsafe_allow_html=True)
