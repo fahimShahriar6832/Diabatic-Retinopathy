fcd5e03e232bdda480c95362ad9e53149d8c88f8
Fahim Shahriar <74281428+fahimShahriar6832@users.noreply.github.com>
2022-07-29 18:10:39 +0600

    Update image_predictor.py

--- image_predictor.py
+++ image_predictor.py
@@ -13,7 +13,7 @@ with st.sidebar:
                            'OCT Image'],
                           icons=['activity','heart'],
                           default_index=0)
-st.title('Diabetes Retinopathy using ML :heart:')  
+st.title('Diabetes Retinopathy using ML :drop_of_blood:')  
 st.write("Diabetic retinopathy (DR) is an illness occurring in the eye due to increase in blood glucose level.")
 
 
