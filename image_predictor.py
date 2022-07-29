eb2643857977533678f5891903d77469b73cb4c8
Fahim Shahriar <74281428+fahimShahriar6832@users.noreply.github.com>
2022-07-29 17:49:09 +0600

    Update image_predictor.py

--- image_predictor.py
+++ image_predictor.py
@@ -56,9 +56,9 @@ if (selected == 'Fundas Image'):
         result = loaded_model.predict(im)
 
         if result[0][0] > result[0][1]:
-          st.write("Diabetic Retinopathy :sad: [{:.2f}% accuracy]".format((result[0][0]*100)))
+          st.write("Diabetic Retinopathy :heart: [{:.2f}% accuracy]".format((result[0][0]*100)))
         else:
-          st.write("NO Diabetic Retinopathy :heart:[{:.2f}% accuracy]".format((result[0][1])*100))
+          st.write("NO Diabetic Retinopathy :thumbs_up:[{:.2f}% accuracy]".format((result[0][1])*100))
 
         
         
